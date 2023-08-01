3c0fecec19d45608593a116454e364ca828d8bde
vitindev <victtorhugo.mendes@gmail.com>
2023-08-01 11:03:45 -0300

    Lista de exercicios 01/08

---
+++ Queue.h
@@ -0,0 +1,60 @@
+#define MAX_SIZE 5
+
+typedef struct {
+    int item[MAX_SIZE];
+} Stack;
+
+Stack queue;
+int top;
+
+void initStack() {
+    top = -1;
+}
+
+int empty() {
+    return top == -1;
+}
+
+int full() {
+    return top == (MAX_SIZE - 1);
+}
+
+void push(Stack *stack, int item) {
+
+    if (full()) {
+        printf("Pilha cheia!");
+        exit(EXIT_FAILURE);
+    }
+
+    stack->item[top++] = item;
+}
+
+int pop(Stack *stack) {
+
+    if (empty()) {
+        printf("\n Pilha vazia!");
+        exit(EXIT_FAILURE);
+    }
+
+    return stack->item[top--];
+}
+
+void call(){
+
+    initStack();
+
+    push(&queue, 10);
+    push(&queue, 20);
+    push(&queue, 30);
+    push(&queue, 40);
+    push(&queue, 50);
+
+    printf("\nTopo: %d", top);
+
+    pop(&queue);
+    pop(&queue);
+    pop(&queue);
+
+    printf("\nTopo: %d", top);
+
+}
--- main.c
+++ main.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include "BankSimple.h"
 #include "Pointer.h"
+#include "Queue.h"
 
 int main() {
 
     //MALLOC SEMPRE VERIFICAR SE NÃO RETORNOU O VALOR COMO 0 NO PONTEIRO, PARA TRATAR O ERRO.
     //FREE LIBERAR A MEMORIA
-    ex1();
+    call();
 
 }
 
